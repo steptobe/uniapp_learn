00c52f6dd2d55798609732bfe8ffcf4019298e25
yuling <670974983@qq.com>
2019-11-25 01:16:37 +0800

    uniapp

---
+++ pages/info/info.vue
@@ -0,0 +1,42 @@
+<template>
+	<view>
+		<view class="title">
+			{{title}}
+		</view>
+		<view class="art-content">
+			<rich-text class="rich" :nodes="strings"></rich-text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title:'',
+				strings:''
+			}
+		},
+		onLoad(e){
+			console.log(e);
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news/36kr/' + e.newsid,
+				method: 'GET',
+				data: {},
+				success: res => {
+					this.title = res.data.title;
+					this.strings = res.data.content;
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
